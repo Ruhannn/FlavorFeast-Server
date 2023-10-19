4a17349e6dae339e3b58084fb4cddf63a3f642ca
Ruhannn <kamiayaka09@gmail.com>
2023-10-19 22:11:27 +0600

    implemented add product feature

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.llm45p4.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,11 +28,36 @@ async function run() {
       res.send(result);
     });
     app.get("/submit-form", async (req, res) => {
-      const cursor = foodCollection.find()
+      const cursor = foodCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
-
+    });
+    app.get("/product-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodCollection.findOne(query);
+      res.send(result);
+    });
+    app.put("/product-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const Product = req.body;
+      const update2 = {
+        $set: {
+          name: Product.name,
+          brandName: Product.brandName,
+          category: Product.category,
+          type: Product.type,
+          price: Product.price,
+          description: Product.description,
+          rating: Product.rating,
+          url: Product.url,
+        },
+      };
+      const result = await foodCollection.updateOne(filter, update2, options);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
