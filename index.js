e8b8266f08676be2048a161d2628326213df43a8
Ruhannn <kamiayaka09@gmail.com>
2023-10-20 17:54:40 +0600

    vercel deployment done

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.use(cors());
 app.use(express.json());
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.llm45p4.mongodb.net/?retryWrites=true&w=majority`;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -18,8 +17,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
     const foodCollection = client.db("FlavorFeast").collection("foods");
     const cartsCollection = client.db("FlavorFeast").collection("carts");
 
@@ -28,14 +25,12 @@ async function run() {
       const result = await foodCollection.insertOne(formData);
       res.send(result);
     });
-
     app.post("/cart", async (req, res) => {
       const cart = req.body;
       console.log(cart);
       const result = await cartsCollection.insertOne(cart);
       res.send(result);
     });
-
     app.get("/cart", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -43,13 +38,13 @@ async function run() {
       res.send(result);
     });
 
+    
     app.get("/product-details/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await foodCollection.findOne(query);
       res.send(result);
     });
-
     app.get("/submit-form", async (req, res) => {
       const cursor = foodCollection.find();
       const result = await cursor.toArray();
@@ -83,15 +78,11 @@ async function run() {
       const result = await foodCollection.updateOne(filter, update2, options);
       res.send(result);
     });
-
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
